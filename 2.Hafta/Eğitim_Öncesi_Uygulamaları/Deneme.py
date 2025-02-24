4d5b41445b8fc928126ff6ed5d3006593f51c656
Buğra Sarıkaya <135698191+Bukooooo@users.noreply.github.com>
2025-02-24 21:03:29 +0300

    Create Deneme.py

---
+++ 2.Hafta/Eğitim_Öncesi_Uygulamaları/Deneme.py
@@ -0,0 +1,24 @@
+print('Hello World')
+
+print("Hello World")
+
+print('''Hello World''')
+
+print("""Hello World""")
+
+print("Ankara","İstanbul","Antalya", sep="/")
+print("Hello","World", sep="")
+print("Hello","World", sep=":)")
+
+#print("Yetgen bünyesinde bulunmak çok faydalı", end=".")
+
+print("Yetgen python programında ""Antalyalı"," Ankaralı ""ve ""İstanbullu ""üyeler bulunmaktadır", sep=",", end=".")
+
+'''
+
+Yukarıda sep ve end parametrelerini beraber kullanmaya çalıştım. :)
+
+'''
+
+x = input()
+print(x)
